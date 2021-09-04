f043bd1b06153398268a263b48e4766f02cc2763
Styrfire <g_n_rogers1991@yahoo.com>
2021-09-04 11:57:02 -0500

    Updated Google Sheets Service to send tire temps and treads (fixed spreadsheet in Google)

--- src/main/groovy/com/stintAnalyzer/service/GoogleSheetsService.java
+++ src/main/groovy/com/stintAnalyzer/service/GoogleSheetsService.java
@@ -147,44 +147,89 @@ public class GoogleSheetsService
 				.setIncludeGridData(true).execute();
 		List<GridData> gridData = spreadsheetWithGrid.getSheets().get(trackSheet.getProperties().getIndex()).getData();
 
-		// get row B2:I2
-		RowData rowB2ToI2 = gridData.get(0).getRowData().get(1);
-		for (int i = 1; i < rowB2ToI2.getValues().size(); i++)
+		Map<Integer, String> mergedColumnLetterMap = new HashMap<>();
+		mergedColumnLetterMap.put(1, "BC");
+		mergedColumnLetterMap.put(2, "DE");
+		mergedColumnLetterMap.put(3, "FG");
+		mergedColumnLetterMap.put(4, "HI");
+		mergedColumnLetterMap.put(5, "JK");
+		mergedColumnLetterMap.put(6, "LM");
+		mergedColumnLetterMap.put(7, "NO");
+		mergedColumnLetterMap.put(8, "PQ");
+
+		// get row B2:C2:P2:Q2
+		RowData rowB2C2ToP2Q2 = gridData.get(0).getRowData().get(1);
+		for (int i = 1; i < rowB2C2ToP2Q2.getValues().size(); i++)
 		{
 			// if this column is free, update its data!
-			if (rowB2ToI2.getValues().get(i).getEffectiveValue() == null)
+			if (rowB2C2ToP2Q2.getValues().get(i).getEffectiveValue() == null)
 			{
-				// get current column letter
-				String column = String.valueOf("ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(i));
+				// get current column letters for merged cell
+				String mergeColumn1 = String.valueOf(mergedColumnLetterMap.get(i).charAt(0));
+				String mergeColumn2 = String.valueOf(mergedColumnLetterMap.get(i).charAt(1));
 
-				// get cell values and fill up to 23 with empty
-				List<Object> cellValues = new ArrayList<>();
-				cellValues.add(stint.getSetupName());
-				cellValues.addAll(stint.getStintLapTimes());
-				for (int j = cellValues.size(); j < 23; j++)
-					cellValues.add("");
-				cellValues.add(stint.getTires().getLeftFront().getTreadRemaining());
-				cellValues.add(stint.getTires().getRightFront().getTreadRemaining());
-				cellValues.add(stint.getTires().getLeftRear().getTreadRemaining());
-				cellValues.add(stint.getTires().getRightRear().getTreadRemaining());
+				// set cell values with setup name and lap times, and fill the rest (up to 21) with empty
+				List<Object> cellValuesLapTimes = new ArrayList<>();
+				cellValuesLapTimes.add(stint.getSetupName());
+				cellValuesLapTimes.addAll(stint.getStintLapTimes());
+				for (int j = cellValuesLapTimes.size(); j < 21; j++)
+					cellValuesLapTimes.add("");
 
 				// add cell values to 2d value array
-				List<List<Object>> values = new ArrayList<>();
-				for (Object cellValue : cellValues)
+				List<List<Object>> valuesLapTimes = new ArrayList<>();
+				for (Object cellValue : cellValuesLapTimes)
 				{
 					List<Object> item = new ArrayList<>();
 					item.add(cellValue);
-					values.add(item);
+					valuesLapTimes.add(item);
 				}
 
 				// shove the data in the spreadsheet at the appropriate column
-				ValueRange valueRange = new ValueRange();
-				valueRange.setValues(values);
-				Sheets.Spreadsheets.Values.Update request = sheetsService.spreadsheets().values()
+				ValueRange valueRangeLapTimes = new ValueRange();
+				valueRangeLapTimes.setValues(valuesLapTimes);
+				Sheets.Spreadsheets.Values.Update requestLapTimes = sheetsService.spreadsheets().values()
 						.update(spreadsheet.getSpreadsheetId(),
-								"'" + trackSheet.getProperties().getTitle() + "'!" + column + "2:" + column + "28", valueRange);
-				request.setValueInputOption("RAW").execute();
-				System.out.println("Added sprint on column " + column + " in the " + stint.getTrackName() + " sheet!");
+								"'" + trackSheet.getProperties().getTitle() + "'!" + mergeColumn1 + "2:" + mergeColumn2 + "22", valueRangeLapTimes);
+				requestLapTimes.setValueInputOption("RAW").execute();
+
+				// set cell values with tire temps and treads
+				List<List<Object>> cellValuesTires = new ArrayList<>();
+				List<Object> leftRightTires = new ArrayList<>();
+				leftRightTires.add(stint.getTires().getLeftFront().getLastTempsOMI());
+				leftRightTires.add(stint.getTires().getRightFront().getLastTempsOMI());
+				cellValuesTires.add(leftRightTires);
+				leftRightTires = new ArrayList<>();
+				leftRightTires.add(stint.getTires().getLeftRear().getLastTempsOMI());
+				leftRightTires.add(stint.getTires().getRightRear().getLastTempsOMI());
+				cellValuesTires.add(leftRightTires);
+				leftRightTires = new ArrayList<>();
+				leftRightTires.add(stint.getTires().getLeftFront().getTreadRemaining());
+				leftRightTires.add(stint.getTires().getRightFront().getTreadRemaining());
+				cellValuesTires.add(leftRightTires);
+				leftRightTires = new ArrayList<>();
+				leftRightTires.add(stint.getTires().getLeftRear().getTreadRemaining());
+				leftRightTires.add(stint.getTires().getRightRear().getTreadRemaining());
+				cellValuesTires.add(leftRightTires);
+
+				// add cell values to 2d value array
+				List<List<Object>> valuesTires = new ArrayList<>();
+				for (List<Object> cellValue : cellValuesTires)
+				{
+					List<Object> item = new ArrayList<>();
+					item.add(cellValue.get(0)); // left tire
+					item.add(cellValue.get(1)); // right tire
+					valuesTires.add(item);
+				}
+
+				// shove the data in the spreadsheet at the appropriate column
+				ValueRange valueRangeTires = new ValueRange();
+				valueRangeTires.setValues(valuesTires);
+				Sheets.Spreadsheets.Values.Update requestTires = sheetsService.spreadsheets().values()
+						.update(spreadsheet.getSpreadsheetId(),
+								"'" + trackSheet.getProperties().getTitle() + "'!" + mergeColumn1 + "25:" + mergeColumn2 + "28", valueRangeTires);
+				requestTires.setValueInputOption("RAW").execute();
+
+				System.out.println("\nAdded sprint on column " + mergeColumn1 + " in the " + stint.getTrackName() + " sheet!\n");
 
 				foundEmptyRow = true;
 				break;
